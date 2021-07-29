c6807916eca34a01942ddf725550ee2c05b88636
pendulumswing <jason@pendulumswingmedia.com>
2021-07-28 20:34:54 -0700

    add react

--- app/client.py
+++ app/client.py
@@ -6,6 +6,6 @@ from flask import Blueprint, render_template
 client_bp = Blueprint('client_app', __name__,
                       url_prefix='',
                       static_url_path='',
-                      static_folder='./dist/static/',
-                      template_folder='./dist/',
+                      static_folder='./build/static/',
+                      template_folder='./build/',
                       )
