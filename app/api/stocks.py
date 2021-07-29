65f8a359416c8d34da89d5611664e27edc19d4de
pendulumswing <jason@pendulumswingmedia.com>
2021-07-28 20:36:58 -0700

    Update stocks.py

--- app/api/stocks.py
+++ app/api/stocks.py
@@ -8,30 +8,30 @@ import requests
 from bs4 import BeautifulSoup
 
 
-@api_rest.route('/stocks')
-class Stocks(Resource):
-
-    # Find all tasks
-    def get(self):
-        return find_all('tasks'), 200
-
-    # Insert task
-    def post(self):
-        data = request.json
-        conn = get_connection()
-        with conn.cursor() as cursor:
-            cursor.execute(f"""INSERT INTO tasks
-                           (list, name, description, creator)
-                           VALUES ('{data['list']}',
-                           '{data['name']}',
-                           '{data['description']}',
-                           '{data['creator']}')
-                           RETURNING id;""")
-            conn.commit()
-            id = cursor.fetchone()[0]
-            result = find_one('tasks', id)
-
-        return result, 201
+# @api_rest.route('/stocks')
+# class Stocks(Resource):
+#
+#     # Find all tasks
+#     def get(self):
+#         return find_all('tasks'), 200
+#
+#     # Insert task
+#     def post(self):
+#         data = request.json
+#         conn = get_connection()
+#         with conn.cursor() as cursor:
+#             cursor.execute(f"""INSERT INTO tasks
+#                            (list, name, description, creator)
+#                            VALUES ('{data['list']}',
+#                            '{data['name']}',
+#                            '{data['description']}',
+#                            '{data['creator']}')
+#                            RETURNING id;""")
+#             conn.commit()
+#             id = cursor.fetchone()[0]
+#             result = find_one('tasks', id)
+#
+#         return result, 201
 
 @api_rest.route('/stocks/<string:resource_id>')
 class Stock(Resource):
@@ -53,33 +53,33 @@ class Stock(Resource):
 #         print(price.text)
         return price.text, 200
 
-    # Delete task
-    def delete(self, resource_id):
-        return delete_one('tasks', resource_id)
-
-    # Update task
-    def put(self, resource_id):
-        data = request.json
-        return update_one_task('tasks', data, resource_id)
-
-
-# Update task
-def update_one_task(table, data, id):
-    # Check for NULL value for Creator
-    if(data['creator'] == None):
-        data['creator'] = 'NULL'
-
-    conn = get_connection()
-    query = (f"""UPDATE {table}
-            SET list = {data['list']},
-            name = '{data['name']}',
-            description = '{data['description']}',
-            creator = {data['creator']}
-            WHERE id={id};""")
-
-    with conn.cursor() as cur:
-        cur.execute(query)
-        conn.commit()
-        result = find_one('tasks', id)
-
-    return result, 201
+#     # Delete task
+#     def delete(self, resource_id):
+#         return delete_one('tasks', resource_id)
+#
+#     # Update task
+#     def put(self, resource_id):
+#         data = request.json
+#         return update_one_task('tasks', data, resource_id)
+#
+#
+# # Update task
+# def update_one_task(table, data, id):
+#     # Check for NULL value for Creator
+#     if(data['creator'] == None):
+#         data['creator'] = 'NULL'
+#
+#     conn = get_connection()
+#     query = (f"""UPDATE {table}
+#             SET list = {data['list']},
+#             name = '{data['name']}',
+#             description = '{data['description']}',
+#             creator = {data['creator']}
+#             WHERE id={id};""")
+#
+#     with conn.cursor() as cur:
+#         cur.execute(query)
+#         conn.commit()
+#         result = find_one('tasks', id)
+#
+#     return result, 201
