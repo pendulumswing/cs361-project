2401c5526c9a5f7f07bfc907b7c5bb63f309ab8c
pendulumswing <jason@pendulumswingmedia.com>
2021-07-31 11:08:36 -0700

    fetch stock price from api server

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [currentPrice, setCurrentPrice] = useState(0);
+
+  useEffect(() => {
+    fetch('/api/stocks/NFLX').then(res => res.json()).then(data => {
+      debugger
+      setCurrentPrice(data)
+    })
+  }, [])
+
   return (
     <div className="App">
+      <div className="bg-blue-500 h-10 w-full">    </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p className="">
+          Current stock price for NFLX is ${currentPrice}
         </p>
         <a
           className="App-link"
--- src/index.js
+++ src/index.js
@@ -6,9 +6,6 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div className="bg-blue-500 h-10 w-10">
-
-    </div>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
