ee1b6be60030eb0593a4faee693b954aaf07c3b8
pendulumswing <jason@pendulumswingmedia.com>
2021-07-31 10:23:16 -0700

    add tailwindcss

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <div className="bg-blue-500 h-10 w-10">
+
+    </div>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
