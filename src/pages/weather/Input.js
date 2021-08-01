acc4c9a15bebadfbb169f34e5ff0fd754afc5c3d
pendulumswing <jason@pendulumswingmedia.com>
2021-08-01 12:19:56 -0700

    style GUI

---
+++ src/pages/weather/Input.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default function Input(props) {
+
+  return (
+    <div>
+      {/*<label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>*/}
+      <div className="mt-1 relative rounded-md shadow-sm">
+        <input type="text" name="location1" id="location1"
+          // className="block w-full pr-10 border-red-300 text-red-900 placeholder-red-300 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm rounded-md"
+          className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+          placeholder="city or zip code"
+          // value=""
+          aria-invalid="true"
+          aria-describedby="email-error"
+        />
+          {/*  /!*Heroicon name: solid/exclamation-circle*!/*/}
+          {/*<div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">*/}
+          {/*  <svg className="h-5 w-5 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"*/}
+          {/*       fill="currentColor" aria-hidden="true">*/}
+          {/*    <path fillRule="evenodd"*/}
+          {/*          d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"*/}
+          {/*          clipRule="evenodd"/>*/}
+          {/*  </svg>*/}
+          {/*</div>*/}
+      </div>
+      {/*<p className="mt-2 text-sm text-red-600" id="email-error">Your password must be less than 4 characters.</p>*/}
+    </div>
+  )
+}
+
--- src/pages/weather/Layout.js
+++ src/pages/weather/Layout.js
@@ -3,6 +3,7 @@ import LocationWeatherData from './LocationWeatherData';
 import axios from 'axios';
 import data1 from './location1data.json';
 import data2 from './location2data.json';
+import Input from './Input';
 
 function Layout(props) {
   // const { children } = props
@@ -74,61 +75,104 @@ function Layout(props) {
   }
 
   return (
-    <div>
-      <body className="p-4 px-6">
-        <div className="py-4">
-          <p className="text-4xl text-blue-60 text-left">Weather Comparison</p>
-          <div className="text-sm text-gray-500 text-left">
-            <p>Enter the name or zip code of two cities to compare their weather. </p>
-            <p>(For example, compare the weather between the city you’re in and the one you want to visit.)</p>
-          </div>
+    <div className="p-4 px-6">
+
+      <div className="py-4">
+        {/*Title*/}
+        <p className="text-4xl text-blue-60 text-left">Weather Comparison</p>
+
+        {/*Instructions*/}
+        <div className="text-gray-500 text-left">
+          <p>Enter the name or zip code of two cities to compare their weather. </p>
+          <p className="text-gray-400">(For example, compare the weather between the city you’re in and the one you want to visit)</p>
+        </div>
+      </div>
+
+
+      <div className="flex ">
+
+        {/*Categories Header*/}
+        <div className="w-1/6 py-2">
         </div>
 
-        <div className="flex ">
-          <div className="w-1/6 py-2">
-          </div>
-          <div className="w-1/4 text-gray-500 font-semibold text-left px-4">
+        {/*Location 1 Header*/}
+        <div className="w-1/4 text-left px-4">
+          <div className="text-2xl font-semibold text-gray-500">
             {data1.name || 'Location 1'}
           </div>
-          <div className="w-1/4 text-gray-500 font-semibold text-left px-4">
+          <div className="text-gray-400">
+            (e.g. where are you at?)
+          </div>
+          <Input />
+        </div>
+
+        {/*Location 2 Header*/}
+        <div className="w-1/4 text-left px-4">
+          <div className="text-2xl font-semibold text-gray-500">
             {data2.name || 'Location 2'}
           </div>
-          <div className="w-1/4">
+          <div className="text-gray-400">
+            (e.g. where are you going?)
+          </div>
+          <Input />
+        </div>
+
+        {/*Graph Header*/}
+        <div className="w-1/4 text-left px-4">
+          <div className="text-2xl font-semibold text-gray-500">
             Graph
+          </div>
+          <div className="text-gray-400">
+            <p>How location 2 compares</p>
+            <p>(a visual "fingerprint")</p>
+          </div>
+        </div>
+      </div>
+
+      {/*Categories*/}
+      <div className="flex">
+        <div className="w-1/6 py-2">
+          <div className="text-sm text-gray-500 text-right space-y-2">
+            <p>Temperature (ºF)</p>
+            <p>Temperature (ºC)</p>
+            <p>Humidity (%)</p>
+            <p>Pressure (hPa)</p>
+            <p>Wind Speed (m/s)</p>
+            <p>Wind Direction</p>
+            <p>Wind Gust (m/s)</p>
+            <p>Cloudiness (%)</p>
+            <p>Precipitation</p>
+            <p>Pollen (g/m^3)</p>
           </div>
         </div>
 
-        <div className="flex">
-          <div className="w-1/6 py-2">
-            <div className="text-sm text-gray-500 text-right space-y-2">
-              <p>Temperature (ºF)</p>
-              <p>Temperature (ºC)</p>
-              <p>Humidity (%)</p>
-              <p>Pressure (hPa)</p>
-              <p>Wind Speed (m/s)</p>
-              <p>Wind Direction</p>
-              <p>Wind Gust (m/s)</p>
-              <p>Cloudiness (%)</p>
-              <p>Precipitation</p>
-              <p>Pollen (g/m^3)</p>
-            </div>
-          </div>
-          <div className="w-1/4 px-4">
-            <LocationWeatherData data={data1}/>
-          </div>
-          <div className="w-1/4">
-            <LocationWeatherData data={data2}/>
-          </div>
-          <div className="w-1/4">
-            <p className="">
-              Current stock price for NFLX is ${currentPrice}
-            </p>
-            <p>
-              OpenWeatherKey: {process.env.REACT_APP_OPENWEATHER_KEY.slice()}
-            </p>
-          </div>
+        {/*Location 1*/}
+        <div className="w-1/4 px-4">
+          <LocationWeatherData data={data1}/>
         </div>
-      </body>
+
+        {/*Location 2*/}
+        <div className="w-1/4">
+          <LocationWeatherData data={data2}/>
+        </div>
+
+        {/*Graph*/}
+        <div className="w-1/4 text-gray-400 text-left px-4">
+          <div className="pb-4">
+            <p className="text-blue-300 font-semibold text-xl">TODO</p>
+            <p>Graph Stuff Goes here</p>
+          </div>
+          <p className="text-blue-300 font-semibold text-xl">Proof of Concept Stuff</p>
+          <p className="">
+            <span>Current stock price for NFLX is: </span>
+            <span className="font-semibold">${currentPrice}</span>
+          </p>
+          <p>
+            OpenWeatherKey (shortened): {process.env.REACT_APP_OPENWEATHER_KEY.slice(20)}
+          </p>
+        </div>
+
+      </div>
     </div>
   );
 }
