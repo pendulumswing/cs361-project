40e31148f75af9b8135acfaeb99b9611b25079aa
pendulumswing <jason@pendulumswingmedia.com>
2021-08-12 10:19:18 -0700

    Update LocationWeatherData.js

--- src/pages/weather/LocationWeatherData.js
+++ src/pages/weather/LocationWeatherData.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import _ from 'lodash';
 import Pollen from './Pollen';
 
@@ -20,16 +20,15 @@ function LocationWeatherData(props) {
     <div className="flex">
       <div className="w-full py-2">
         <div className="text-gray-500 text-left space-y-2 h-full whitespace-pre-wrap">
-          <p>{location.main.temp ? `${((location.main.temp) * 1.8 + 32).toFixed(0)}º` : ''}</p>
-          <p>{location.main.temp ? `${((location.main.temp)).toFixed(0)}º` : ' '}</p>
-          <p>{location.main.humidity ? `${location.main.humidity.toFixed(0)}%` : ' '}</p>
-          <p>{location.main.pressure ? `${location.main.pressure.toFixed(0)}hPa` : ' '}</p>
-          <p>{location.wind.speed ? `${location.wind.speed.toFixed(2)}m/s` : ' '}</p>
-          <p>{location.wind.deg ? `${location.wind.deg.toFixed(0)}º` : ' '}</p>
-          <p>{location.wind.gust ? `${location.wind.gust.toFixed(2)}m/s` : ' '}</p>
-          <p>{location.clouds.all ? `${location.clouds.all.toFixed(0)}%` : ' '}</p>
+          <p>{location.main.temp ? `${((location.main.temp) * 1.8 + 32).toFixed(0)}º` : '0º'}</p>
+          <p>{location.main.temp ? `${((location.main.temp)).toFixed(0)}º` : '0º'}</p>
+          <p>{location.main.humidity ? `${location.main.humidity.toFixed(0)}%` : '0%'}</p>
+          <p>{location.main.pressure ? `${location.main.pressure.toFixed(0)}hPa` : '0hPa'}</p>
+          <p>{location.wind.speed ? `${location.wind.speed.toFixed(2)}m/s` : '0m/s'}</p>
+          <p>{location.wind.deg ? `${location.wind.deg.toFixed(0)}º` : '0º'}</p>
+          <p>{location.wind.gust ? `${location.wind.gust.toFixed(2)}m/s` : '0m/s'}</p>
+          <p>{location.clouds.all ? `${location.clouds.all.toFixed(0)}%` : '0%'}</p>
           <Pollen pollen={pollen} />
-          {/*<p>{location.clouds.all ? `${location.clouds.all.toFixed(0)}%` : ' '}</p>*/}
         </div>
       </div>
     </div>
